b6a22e22e0fdeb2ec7b151e05db3371d0d106899
Danny Stewart <danny@stewart.cc>
2025-12-05 11:15:49 -0500

    Initial Commit

---
+++ xDeploy/xDeployApp.swift
@@ -0,0 +1,17 @@
+//
+//  xDeployApp.swift
+//  xDeploy
+//
+//  Created by Danny Stewart on 12/5/25.
+//
+
+import SwiftUI
+
+@main
+struct xDeployApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
